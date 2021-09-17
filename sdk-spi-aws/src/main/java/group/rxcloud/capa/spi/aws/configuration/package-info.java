f963ba04de9243ef0bde04ed5eed69b87a989ded
Kevin_T <596823919@qq.com>
2021-09-17 16:12:28 +0800

    docs: add aws app-config wiki

---
+++ sdk-spi-aws/src/main/java/group/rxcloud/capa/spi/aws/configuration/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * @apiNote https://aws.amazon.com/cn/blogs/china/technical-selection-and-landing-practice-for-building-a-cloud-native-configuration-center/
+ */
+package group.rxcloud.capa.spi.aws.configuration;
