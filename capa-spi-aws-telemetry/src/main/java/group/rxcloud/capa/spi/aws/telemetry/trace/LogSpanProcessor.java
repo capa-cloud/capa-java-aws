5367b3f7066a999843185a1c1054ceefe01d80e0
chenyijiang <chenyijiang@ctrip.com>
2021-12-01 18:50:44 +0800

    Trace and metrics implementation on aws.

---
+++ capa-spi-aws-telemetry/src/main/java/group/rxcloud/capa/spi/aws/telemetry/trace/LogSpanProcessor.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package group.rxcloud.capa.spi.aws.telemetry.trace;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.sdk.common.CompletableResultCode;
+import io.opentelemetry.sdk.common.InstrumentationLibraryInfo;
+import io.opentelemetry.sdk.trace.ReadWriteSpan;
+import io.opentelemetry.sdk.trace.ReadableSpan;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.data.EventData;
+import io.opentelemetry.sdk.trace.data.SpanData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Configured in capa-tracer.json
+ */
+public class LogSpanProcessor implements SpanProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(LogSpanProcessor.class);
+
+    private static StringBuilder addBasicTags(SpanData spanData) {
+        StringBuilder builder = new StringBuilder("[[traceId=").append(spanData.getTraceId());
+        appendTag(builder, "spanId", spanData.getSpanId());
+        appendTag(builder, "parentSpanId", spanData.getParentSpanId());
+        appendTag(builder, "spanName", spanData.getName());
+        appendTag(builder, "status", spanData.getStatus().getStatusCode().name());
+        if (spanData.getKind() != null) {
+            appendTag(builder, "spanKind", spanData.getKind().name());
+        }
+        InstrumentationLibraryInfo libraryInfo = spanData.getInstrumentationLibraryInfo();
+        if (libraryInfo != null) {
+            appendTag(builder, "tracerName", libraryInfo.getName());
+            appendTag(builder, "schemaUrl", libraryInfo.getSchemaUrl());
+            appendTag(builder, "version", libraryInfo.getVersion());
+        }
+        builder.append("]]");
+        return builder;
+    }
+
+    private static void appendTag(StringBuilder builder, String key, String value) {
+        if (key != null && value != null) {
+            builder.append(", ").append(key).append('=').append(value);
+        }
+    }
+
+    private static void appendAttributes(String blankPrefix, StringBuilder builder, Attributes attributes) {
+        builder.append('\n').append(blankPrefix).append("\"attributes\" = {");
+        if (!attributes.isEmpty()) {
+            attributes.forEach((k, v) -> {
+                builder.append("\n\t").append(blankPrefix).append(k.getKey()).append('=').append(v).append(',');
+            });
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        builder.append('\n').append(blankPrefix).append('}');
+    }
+
+    @Override
+    public void onStart(Context parentContext, ReadWriteSpan span) {
+    }
+
+    @Override
+    public boolean isStartRequired() {
+        return false;
+    }
+
+    @Override
+    public void onEnd(ReadableSpan span) {
+        SpanData spanData = span.toSpanData();
+        boolean failed = spanData.getStatus().getStatusCode() == StatusCode.ERROR;
+
+        StringBuilder builder = addBasicTags(spanData);
+        appendAttributes("", builder, spanData.getAttributes());
+        if (!spanData.getLinks().isEmpty()) {
+            builder.append(",\n\"links\" = [");
+            spanData.getLinks().forEach(l -> builder.append("\n\t").append(l).append(','));
+            builder.deleteCharAt(builder.length() - 1);
+            builder.append("\n]");
+        }
+        if (!spanData.getEvents().isEmpty()) {
+            builder.append(",\n\"events\" = [");
+            for (EventData l : spanData.getEvents()) {
+                builder.append("\n\t").append("{\n\t\t\"name\": \"").append(l.getName())
+                       .append("\",\n\t\t\"time\": ")
+                       .append(Instant.ofEpochMilli(TimeUnit.NANOSECONDS.toMillis(l.getEpochNanos())).toString());
+                appendAttributes("\t\t", builder, l.getAttributes());
+                if ("exception".equals(l.getName())) {
+                    failed = true;
+                }
+                builder.append("\n\t},");
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            builder.append("\n]");
+        }
+
+        if (failed) {
+            log.error(builder.toString());
+        } else {
+            log.info(builder.toString());
+        }
+    }
+
+    @Override
+    public boolean isEndRequired() {
+        return true;
+    }
+
+    @Override
+    public CompletableResultCode shutdown() {
+        return CompletableResultCode.ofSuccess();
+    }
+}
---
+++ capa-spi-aws-telemetry/src/test/java/group/rxcloud/capa/spi/aws/telemetry/CloudWatchMetricsDemo.java
@@ -0,0 +1,132 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package group.rxcloud.capa.spi.aws.telemetry;
+
+import group.rxcloud.capa.telemetry.CapaTelemetryClientGlobal;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.BoundDoubleHistogram;
+import io.opentelemetry.api.metrics.BoundLongCounter;
+import io.opentelemetry.api.metrics.BoundLongUpDownCounter;
+import io.opentelemetry.api.metrics.DoubleHistogram;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.LongUpDownCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Demo
+ */
+public class CloudWatchMetricsDemo {
+
+    //@BeforeEach
+    public void before() {
+    }
+
+    //@Test
+    public void trace() {
+        Tracer tracer = CapaTelemetryClientGlobal.getOrCreate().buildTracer("capa").block();
+        Span rootSpan = tracer.spanBuilder("rootSpan")
+                              .setSpanKind(SpanKind.SERVER)
+                              .setAttribute("type", "root")
+                              .startSpan();
+        rootSpan.end();
+
+        Span subSpan = tracer.spanBuilder("subSpan")
+                             .setSpanKind(SpanKind.INTERNAL)
+                             .setAttribute("type", "sub")
+                             .setParent(Context.current().with(rootSpan))
+                             .startSpan();
+        subSpan.addEvent("myEvent", Attributes.of(AttributeKey.stringKey("key"), "value"));
+        subSpan.end();
+    }
+
+    @Test
+    public void metrics() throws InterruptedException {
+        // init client.
+
+        Meter meter = CapaTelemetryClientGlobal.getOrCreate().buildMeter("capa").block();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        // gauge
+        meter.gaugeBuilder("gauge_test")
+             .ofLongs()
+             .buildWithCallback(observer -> {
+                 int value = new Random().nextInt(100);
+                 System.out.println("gauge " + value);
+                 observer.observe(value);
+             });
+
+        executorService.submit(() -> {
+            // histogram
+            Random random = new Random();
+
+            DoubleHistogram doubleHistogram = meter.histogramBuilder("histogram_test")
+                                                   .build();
+            BoundDoubleHistogram boundDoubleHistogram = doubleHistogram
+                    .bind(Attributes.of(AttributeKey.stringKey("type"), "histogram"));
+            for (int i = 0; i < 5000; i++) {
+                int num = random.nextInt(50);
+                doubleHistogram.record(num);
+                boundDoubleHistogram.record(num + 2);
+                //System.out.println("histogram " + num);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        // counter
+        // counter with different attrs will counting seperately. attrs will be attached to data points.
+        LongCounter counter = meter.counterBuilder("counter_test")
+                                   .build();
+
+        BoundLongCounter boundLongCounter1 = counter.bind(
+                Attributes.of(AttributeKey.stringKey("type"), "id_1"));
+        BoundLongCounter boundLongCounter2 = counter.bind(Attributes.of(AttributeKey.stringKey("type"), "id_2"));
+
+        LongUpDownCounter upDownCounter = meter.upDownCounterBuilder("counter_test")
+                                               .build();
+        BoundLongUpDownCounter boundLongUpDownCounter = upDownCounter
+                .bind(Attributes.of(AttributeKey.stringKey("type"), "dec"));
+
+        Random random = new Random();
+        for (int i = 0; i < 120; i++) {
+            int num1 = random.nextInt(5);
+            int num2 = random.nextInt(5);
+            boundLongCounter1.add(num1);
+            boundLongCounter2.add(num2);
+            boundLongUpDownCounter.add(num1);
+            boundLongUpDownCounter.add(-num2);
+            Thread.sleep(1000);
+        }
+
+        Thread.sleep(10 * 60 * 1000);
+    }
+}
